d9a0e3feffd01df9e7b3fdd0079dfc9f4517ee03
growthstepsgs <growthstepsgs@gmail.com>
2026-07-30 22:41:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -452,9 +452,7 @@ def sitemap():
     return Response("\n".join(lines), mimetype="application/xml")
 
 #======================robot.txt====================
-@app.route("/robots.txt")
-def robots():
-    return send_from_directory("static", "robots.txt")
+
 
 @app.route("/admin/cleanup-orphaned-photos")
 @admin_required
